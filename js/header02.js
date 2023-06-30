ec82dd15f60d250b3114e56fb509ff23bc42b487
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-06-30 14:25:36 +0900

    230630
    230630

--- js/header02.js
+++ js/header02.js
@@ -1,10 +1,16 @@
 $(function () {
+
+    // $(window).resize(function () {
+    //     if ($(window).width() > 768) {
+    //         $('#header .gnb').removeClass('on');
+    //     }
+    // });
+
     $('#header .gnb>ul>li').on('click', function (e) {
         if ($('#header .gnb').hasClass('on')) {
             e.preventDefault();
             $(this).addClass('on').siblings().removeClass('on');
         }
-        // w768이상일때 on을 지워주면 되는거같은디
     });
     $('.mopen').on('click', function () {
         $('#header .gnb').toggleClass('on');
